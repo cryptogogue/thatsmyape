9363007df5279ff70a83a22631095cce1ca2868b
Patrick Meehan <patrickmeehan.git@fallguyconsulting.com>
2021-11-30 20:26:41 -0800

    bah. typo

--- src/content.js
+++ src/content.js
@@ -7,7 +7,7 @@ export const CONTENT_INTRODUCTION = (`
 
 Does owning an NFT grant you any real ownership of the artwork it represents? Do transactions on a blockchain represent a legally actionable chain of custody? Does having a license for "personal" or even "commercial" use of an artwork associated with an NFT enable you to send DMCA takedown notices, or do those rights reside with the original author of the artwork? Are "double spends" of intellectual property licenses really a problem that needs an expensive technical solution, or is simple contract law enough? Are "smart contracts" legally robust enough to stand up in a court of law? And, since blockchains are trustless and do not inherently convey the transfer of rights in any legally meaningful way, do you need a blockchain at all?
 
-More importantly, are you sick of jerks rick-clicking to steal your sweet, sweet NFTs?
+More importantly, are you sick of jerks right-clicking to steal your sweet, sweet NFTs?
 
 That's why we made *That's My Ape!*, an entirely **blockchain-free** cryptographic chain of custody tool that runs locally in your browser.
 
